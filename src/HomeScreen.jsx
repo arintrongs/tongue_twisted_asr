160b016e0b40b6b2d6e8caaf7d3f8b8315f38be7
mild <mild@Milds-MacBook-Pro.local>
2019-05-02 12:03:18 +0700

    add bg

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -5,12 +5,14 @@ import logo from './assets/img/logo.png'
 class HomeScreen extends React.Component {
   render() {
     return (
-      <div className="home-container">
-        {/* <div className="logo"> */}
-        {/* <img className="logo" src={logo} /> */}
-        {/* </div> */}
-        <div className="start">กดปุ่มใดๆ เพื่อเริ่มเกม</div>
-      </div>
+      <React.Fragment>
+        <div className="home-container">
+          <div className="title"> ทั้งก์ ทวิสต์</div>
+          <img className="logo" src={logo} />
+          <div className="start">กดปุ่ม "เอ็นเทอร์" เพื่อเริ่มเกม</div>
+        </div>
+        <div className="background" />
+      </React.Fragment>
     )
   }
 }
