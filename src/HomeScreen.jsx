e8295ec0597f06646ba2e4568264aff1ed0f3236
mild <mild@Milds-MacBook-Pro.local>
2019-05-02 12:13:51 +0700

    fix conflict

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -5,14 +5,14 @@ import logo from './assets/img/logo.png'
 class HomeScreen extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="fixed-container">
         <div className="home-container">
           <div className="title"> ทั้งก์ ทวิสต์</div>
           <img className="logo" src={logo} />
           <div className="start">กดปุ่ม "เอ็นเทอร์" เพื่อเริ่มเกม</div>
         </div>
         <div className="background" />
-      </React.Fragment>
+      </div>
     )
   }
 }
