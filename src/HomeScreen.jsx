af1a2db70f063b89655aff3053ffd27d858150d8
mild <mild@Milds-MacBook-Pro.local>
2019-05-02 11:32:39 +0700

    add character

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './HomeScreen.css'
+import logo from './assets/img/logo.png'
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <div className="home-container">
-        <div className="logo">Logo</div>
+        {/* <div className="logo"> */}
+        {/* <img className="logo" src={logo} /> */}
+        {/* </div> */}
         <div className="start">กดปุ่มใดๆ เพื่อเริ่มเกม</div>
       </div>
     )
